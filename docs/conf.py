fc1b1ee5e89dbac37010032e1f9710568d071446
aotte <86734724+aelinrowan@users.noreply.github.com>
2023-04-20 14:46:57 -0400

    installing furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,19 @@ author = 'Andrea'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autoclass_content = 'both'
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
